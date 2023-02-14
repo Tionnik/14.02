cdc9dc4ccb959bfef4c4cb1ea955f74295495fdd
Nik <ice_fire50@mail.ru>
2023-02-14 15:02:37 +0300

    new dz

---
+++ Task 10/Program.cs
@@ -0,0 +1,17 @@
+﻿//int N = new Random().Next(100,1000);
+//int q1 = N/100;
+//int q2 = N%10;
+//Console.WriteLine(N + " -> " + q1 + q2);
+
+//Console.WriteLine("Введите первое число");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите второе число");
+//int b = int.Parse(Console.ReadLine());
+//int ost = a%b;
+//if (ost == 0) Console.WriteLine("кратно");
+//else Console.WriteLine("не кратно, Остаток "+ost);
+
+//Console.WriteLine("Введите первое число");
+//int a = int.Parse(Console.ReadLine());
+//if (a % 7==0 && a%23==0) Console.WriteLine("Кратно 7 и 23");
+//else Console.WriteLine("не Кратно 7 и 23");

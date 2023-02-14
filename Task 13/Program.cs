3357a0ea5eb104944ef28604e4c55950af17c8a0
Nik <ice_fire50@mail.ru>
2023-02-14 15:15:00 +0300

    Task 13

---
+++ Task 13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+//Console.WriteLine("Введите первое число");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите второе число");
+//int b = int.Parse(Console.ReadLine());
+//int ost = a%b;
+//if (ost == 0) Console.WriteLine("кратно");
+//else Console.WriteLine("не кратно, Остаток "+ost);
+
+//Console.WriteLine("Введите первое число");
+//int a = int.Parse(Console.ReadLine());
+//if (a % 7==0 && a%23==0) Console.WriteLine("Кратно 7 и 23");
+//else Console.WriteLine("не Кратно 7 и 23");
